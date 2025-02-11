04697f460730053dd55a71cbb202ac6cc75f4a1f
AEMuto <antoine.marseaud@gmail.com>
2025-02-12 00:19:55 +0100

    Refactor _Hero.tsx to use path aliases and implement language support for text content

--- src/views/Home/_Hero.tsx
+++ src/views/Home/_Hero.tsx
@@ -1,49 +1,70 @@
 import styled from "@emotion/styled";
-import Subheading from "../../components/Subheading";
-import Heading from "../../components/Heading";
-import { SlideColumn, SlideContainer } from "../../components/Slide";
-import Button from "../../components/Button";
+import Subheading from "@components/Subheading";
+import Heading from "@components/Heading";
+import { SlideColumn, SlideContainer } from "@components/Slide";
+import Button from "@components/Button";
 import { Link } from "react-router-dom";
-import Portrait from "../../../assets/images/me.jpg";
-import Typography from "../../components/Typography";
+import Portrait from "@assets/images/me.jpg";
+import Typography from "@components/Typography";
+import { useLanguage } from "@contexts/LanguageContext";
 
 const Hero = () => {
+  const { t } = useLanguage();
   return (
     <SlideContainer id="header" isHeader={true}>
       <SlideColumn>
         <Subheading size="lg" bold>
-          Bonjour Interweb&nbsp;!
+          {t({ fr: "Bonjour Interweb\u00A0!", en: "Hello Interweb!" })}
         </Subheading>
         <Heading size="xxl">
-          Je m’appelle
+          {t({ fr: "Je m’appelle", en: "My name is" })}
           <br />
           <span data-color="primary">Antoine Marseaud</span>
         </Heading>
         <Typography size="lg">
-          Je suis quelqu'un d'enthousiaste qui aime résoudre des problèmes grâce au code. Ma plus
-          grande source de motivation est la construction de solutions performantes, élégantes et
-          facile d'utilisation pour tout le monde.
+          {t({
+            fr: `Je suis quelqu'un d'enthousiaste qui aime résoudre des problèmes grâce au code. Ma plus grande source de motivation est la construction de solutions performantes, élégantes et facile d'utilisation pour tout le monde.`,
+            en: `I am an enthusiastic person who loves solving problems through code. My greatest source of motivation is building performant, elegant solutions that are easy to use for everyone.`,
+          })}
         </Typography>
         <Typography size="lg">
-          <b>Spécialisé dans le développement web</b>, les dernières technologies que j'ai utilisées
-          incluent de manière non-exhaustive&nbsp;:{" "}
-          <b>React, Typescript, NodeJS, Vue, Appwrite, MongoDB.</b>
+          {t({
+            fr: (
+              <>
+                <b>Spécialisé dans le développement web</b>, les dernières technologies que j'ai
+                utilisées incluent de manière non-exhaustive&nbsp;:{" "}
+                <b>React, Typescript, NodeJS, Vue, Appwrite, MongoDB.</b>
+              </>
+            ),
+            en: (
+              <>
+                <b>Specialized in web development</b>, the latest technologies I've used include but
+                are not limited to: <b>React, Typescript, NodeJS, Vue, Appwrite, MongoDB.</b>
+              </>
+            ),
+          })}
         </Typography>
         <CTAContainer>
-          <Link to="/#projects" title="Voir mes projets">
+          <Link
+            to="/#projects"
+            title={t({ fr: "Voir mes projets", en: "View Projects" }) as string}>
             <Button>Portfolio</Button>
           </Link>
           <a
+            //TODO: ternary for the href (need english and french resume)
             href="https://drive.google.com/file/d/1-FfXA6LFPYDRbya9_Bi5qBTz1xgBWB-_/view?usp=sharing"
-            title="Pour consulter mon CV"
+            title={t({ fr: "Consulter mon CV", en: "View Resume" }) as string}
             target="_blank"
             rel="noreferrer">
-            <Button option="alternate">Consulter mon CV</Button>
+            <Button option="alternate">{t({ fr: "Consulter mon CV", en: "View Resume" })}</Button>
           </a>
         </CTAContainer>
       </SlideColumn>
       <StyledSlideColumn>
-        <StyledImg src={Portrait} alt="My Portrait" />
+        <StyledImg
+          src={Portrait}
+          alt={t({ fr: "Photo de profil", en: "Profile picture" }) as string}
+        />
       </StyledSlideColumn>
     </SlideContainer>
   );
