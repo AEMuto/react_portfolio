19e2337a184829428773c98257c39fc4c2a92b0d
AEMuto <antoine.marseaud@gmail.com>
2025-02-11 19:45:50 +0100

    Refactor import paths in _About.tsx to use path aliases for improved readability

--- src/views/Home/_About.tsx
+++ src/views/Home/_About.tsx
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
-import { SlideColumn, SlideContainer } from "../../components/Slide";
-import Heading from "../../components/Heading";
-import AboutMarkdown from "../../data/markdown/about.mdx";
-import Typography from "../../components/Typography";
-import ExternalLink from "../../components/ExternalLink";
+import { SlideColumn, SlideContainer } from "@components/Slide";
+import Heading from "@components/Heading";
+import AboutMarkdown from "@data/markdown/about.mdx";
+import Typography from "@components/Typography";
+import ExternalLink from "@components/ExternalLink";
 
 const StyledTypography = styled(Typography)`
   @media screen and (max-width: 768px) {
